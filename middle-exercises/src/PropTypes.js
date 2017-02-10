0f1d1b06338150a2830fcd4bc6ec2b4188b3d721
Antony O'Neill <antony@mention-me.com>
2017-02-10 11:57:06 +0000

    proptypes exercise

--- middle-exercises/src/PropTypes.js
+++ middle-exercises/src/PropTypes.js
@@ -1,11 +1,11 @@
-import React, { Component, PropTypes } from 'react'
+import React, {Component, PropTypes} from 'react'
 
 export default class PropTypesExercise extends Component {
   render() {
     return (
-      <div>
-        <HelloPerson />
-        <OtherHelloPerson />
+      <div >
+        <HelloPerson/>
+        <OtherHelloPerson name="Jasper"/>
       </div>
     )
   }
@@ -14,24 +14,23 @@ export default class PropTypesExercise extends Component {
 class HelloPerson extends Component {
   render() {
     return (
-      <p>Hello, { this.props.name }</p>
+      <p>Hello, {this.props.name}</p>
     )
   }
 }
 
 HelloPerson.propTypes = {
-  // EXERCISE: fill this out with the right propTypes such that we see
-  // an error if we forget to pass a name property
+  name: PropTypes.string.isRequired
 }
 
 HelloPerson.defaultProps = {
-  // EXERCISE: fill this out so that if we don't pass the name prop, it defaults
-  // to "JacK" (or your own name, I'm easy!)
+  name: "Antony"
 }
 
 const OtherHelloPerson = props => (
-  <p>OtherHello, { props.name }</p>
+  <p>OtherHello, {props.name}</p>
 )
 
-// EXERCISE: how can we still declare propTypes and default props for functional,
-// stateless components?
+OtherHelloPerson.propTypes = {
+  name: PropTypes.string.isRequired
+}
