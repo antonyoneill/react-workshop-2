125f072a4f6e33d5ee9fed0b5212f8b2dc457a76
Antony O'Neill <antony@mention-me.com>
2017-02-10 16:09:38 +0000

    create an about page

---
+++ react-router-exercises/src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <p>Here is the about page</p>
+      </div>
+    )
+  }
+}
+
+export default About
