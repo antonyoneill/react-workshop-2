cf6ffc479f7065966f4f61cc4105a082abea93bb
Antony O'Neill <antony@mention-me.com>
2017-02-10 11:41:22 +0000

    satisfy the es2015 tests!

--- es2015-exercises/es2015.test.js
+++ es2015-exercises/es2015.test.js
@@ -1,6 +1,6 @@
 test('you can use arrow functions instead of regular functions', () => {
   // swap this func for an arrow function
-  const func = function() {
+  const func = () => {
     return 3
   }
 
@@ -9,9 +9,7 @@ test('you can use arrow functions instead of regular functions', () => {
 
 test('one line arrow functions do not need a return', () => {
   // turn this function into an arrow function that is on a single line
-  const func = () => {
-    return 3
-  }
+  const func = () => 3;
 
   expect(func()).toEqual(3)
 })
@@ -24,9 +22,9 @@ test('arrow functions always implicitly bind to the correct scope', () => {
       // change this function to an arrow function
       // and remove the bind(this)
       // and see what happens
-      return this.friends.map(function(friend) {
+      return this.friends.map((friend) => {
         return `${this.name} has ${friend} as a friend`
-      }.bind(this))
+      })
     }
   }
 
@@ -41,12 +39,15 @@ test('classes can have instance methods', () => {
     bar() {
       return 'bar'
     }
+    hello() {
+      return 'world'
+    }
   }
 
   expect(new Foo().bar()).toEqual('bar')
 
   // uncomment the test below and get it ot pass
-  // expect(new Foo().hello()).toEqual('world')
+  expect(new Foo().hello()).toEqual('world')
 })
 
 test('classes can have static class methods', () => {
@@ -57,10 +58,12 @@ test('classes can have static class methods', () => {
     return 'bar'
   }
 
+  Foo.hello = {a: 1}
+
   expect(Foo.bar()).toEqual('bar')
 
   // get the below test to pass
-  // expect(Foo.hello).toEqual({ a: 1 })
+  expect(Foo.hello).toEqual({ a: 1 })
 })
 
 test('classes can have a constructor which is called when they are created', () => {
@@ -68,18 +71,23 @@ test('classes can have a constructor which is called when they are created', () 
     constructor(name) {
       this.name = name
     }
+
+    getName() {
+      return this.name
+    }
   }
 
   const jack = new Person('jack')
   expect(jack.name).toEqual('jack')
 
   // exercise: uncomment this test and get it to pass
-  // expect(jack.getName()).toEqual('jack')
+  expect(jack.getName()).toEqual('jack')
 })
 
 test('you can map over arrays', () => {
   const start = [1, 2, 3]
-  const mapped = start.map(x => x)
+  const mapped = start.map((x) => x * 2 )
+
   // make the below test pass
-  // expect(mapped).toEqual([2, 4, 6])
+  expect(mapped).toEqual([2, 4, 6])
 })
