c9d1f6ca7f4524966a079781abff7ab080080649
Antony O'Neill <antony@mention-me.com>
2017-02-10 14:08:55 +0000

    don't use context!

--- middle-exercises/src/Context.js
+++ middle-exercises/src/Context.js
@@ -6,6 +6,7 @@ export default class ContextExercise extends Component {
   getChildContext() {
     return {
       secretValue: 'bar',
+      shouldUseContext: 'no!'
     }
   }
   render() {
@@ -19,15 +20,17 @@ export default class ContextExercise extends Component {
 
 ContextExercise.childContextTypes = {
   secretValue: PropTypes.string.isRequired,
+  shouldUseContext: PropTypes.string.isRequired
 }
 
 class SomeChildComponent extends Component {
   render() {
     return (
-      <p>Context value: { this.context.secretValue }</p>
+      <p>Context value: { this.context.secretValue }. Should we use context? {this.context.shouldUseContext}</p>
     )
   }
 }
 SomeChildComponent.contextTypes = {
   secretValue: PropTypes.string.isRequired,
+  shouldUseContext: PropTypes.string.isRequired
 }
